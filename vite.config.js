76ecba417ed5ed7330aa98c5952e384e34bc8d9f
Melvin Kempe <k3mp314@gmail.com>
2023-02-11 15:38:43 +0100

    [test]

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-    base: "/Portfolio2",
+    base: "/Portfolio",
 
     build: {
         rollupOptions: {
