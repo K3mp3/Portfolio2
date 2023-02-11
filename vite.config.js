b264ef8de72e527e6c02ca2e961d0c619e0b0c04
Melvin Kempe <k3mp314@gmail.com>
2023-02-11 15:31:38 +0100

    [test]

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-    base: "/",
+    base: "https://melvinkempe.com",
 
     build: {
         rollupOptions: {
